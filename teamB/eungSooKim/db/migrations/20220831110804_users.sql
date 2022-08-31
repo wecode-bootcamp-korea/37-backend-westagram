f2a08bf1065f4f85f55435df60e69a35669af03b
es <mascoeskim@gmail.com>
2022-08-31 21:05:16 +0900

    과제1

---
+++ teamB/eungSooKim/db/migrations/20220831110804_users.sql
@@ -0,0 +1,13 @@
+-- migrate:up
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    profile_image VARCHAR(1000) NULL DEFAULT "https://github.com/amacneil/dbmate#command-line-options"
+);
+
+-- migrate:down
+DROP TABLE users;
---
+++ teamB/eungSooKim/db/migrations/20220831111048_posts.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+CREATE TABLE posts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    profile_image VARCHAR(1000) NULL DEFAULT "imageUrl",
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- migrate:down
+DROP TABLE posts;
