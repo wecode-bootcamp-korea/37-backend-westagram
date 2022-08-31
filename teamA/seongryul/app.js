6d65d7b3227982d5646ee5371d6f06ed8e3373e3
SR-Jung <westeeven0612@gmail.com>
2022-08-31 15:59:17 +0900

    멘토 리뷰사항 수정

--- teamA/seongryul/app.js
+++ teamA/seongryul/app.js
@@ -1,13 +1,14 @@
-const http = require ("http");
-const express = require ("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-
 dotenv.config();
+
+const http      = require ("http");
+const express   = require ("express");
+const cors      = require("cors");
+const morgan    = require("morgan");
+const dotenv    = require("dotenv");
 const { DataSource } = require('typeorm')
 
-const myDataSource = new DataSource({
+
+const database = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
@@ -16,12 +17,12 @@ const myDataSource = new DataSource({
     database: process.env.TYPEORM_DATABASE
 })
 
-myDataSource.initialize()
-    .then(() => {
+database.initialize()
+.then(() => {
     console.log("Data Source has been initialized!")
-    })
-
-app = express ();
+})
+const app = express();
+app = express();
 
 app.use(express.json());
 app.use(cors());
