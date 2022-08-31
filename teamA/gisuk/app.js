2a388f5952150f5389dc5917aa7570222d7b4a03
pienk <kisuk623@gmail.com>
2022-08-31 16:24:57 +0900

    EDIT

--- teamA/gisuk/app.js
+++ teamA/gisuk/app.js
@@ -1,14 +1,13 @@
+require('dotenv').config();
 const express = require('express');
-
 const cors = require('cors');
 const morgan = require('morgan');
-
-const dotenv = require('dotenv');
-dotenv.config()
-
 const { DataSource } = require('typeorm');
 
-const myDataSource = new DataSource({
+const app = express();
+const PORT = process.env.PORT
+
+const appDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
@@ -17,45 +16,39 @@ const myDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE
 })
 
-myDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!")
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err)
-    myDataSource.destroy()
-  })
+appDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err)
+      appDataSource.destroy()
+    })
 
-  const app = express();
-  const PORT = process.env.PORT
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+    
+app.get("/ping", (req,res) => {
+  res.status(200).json({"message" : "pong"});
+})
 
-  app.use(cors());
-  app.use(morgan('dev'));
-  app.use(express.json());
+app.get("/users", async (req,res) => {
+  await appDataSource.query(
+    `SELECT 
+    users.name,
+    users.email,
+    users.profile_image
+    FROM users`,
+    (err, rows) => {
+      res.status(200).json(rows);
+    }
+  )
+});
 
-  app.get("/ping", (req,res) => {
-    res.status(200).json({"message" : "pong"});
-  })
-
-  app.get("/users", async (req,res) => {
-    await myDataSource.query(
-      `SELECT 
-      users.name,
-      users.email,
-      users.profile_image
-      FROM users`,
-      (err, rows) => {
-        res.status(200).json(rows);
-      }
-    )
-  });
-
-  app.post("/users", async (req, res, next) => {
-    const { name, email, profile_image, password} = req.body
-  
-    // console.log(req)
-  
-    await myDataSource.query(
+app.post("/users", async (req, res, next) => {
+  const { name, email, profile_image, password} = req.body  
+    await appDataSource.query(
       `INSERT INTO users(
         name,
         email,
