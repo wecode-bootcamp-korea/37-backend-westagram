3bea1418f6c0a9cd326d6bc22ffeecb6e7c84bb1
dyjeong576 <dyjeong576@gmail.com>
2022-08-31 15:23:15 +0900

    assignment 2

--- teamC/JeongDayoung/app.js
+++ teamC/JeongDayoung/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { DataSource } = require("typeorm");
 
 const app = express();
 
@@ -12,8 +13,33 @@ app.use(morgan('dev'));
 
 dotenv.config();
 
+const myDataSource = new DataSource({
+  type : process.env.TYPEORM_CONNECTION,
+  host : process.env.TYPEORM_HOST,
+  port : process.env.TYPEORM_PORT,
+  username : process.env.TYPEORM_USERNAME,
+  password : process.env.TYPEORM_PASSWORD,
+  database : process.env.TYPEORM_DATABASE
+
+}) 
+
+myDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  });
+
 app.get('/ping', cors(), function(req, res, next){
   res.json({message : 'pong'})
+})
+
+app.post("/users", async(req, res, next) => {
+  const {name, email, profile_image, password} = req.body;
+  await myDataSource.query(
+    `INSERT INTO users(name, email, profile_image, password)
+    values(?, ?, ?,?);`,
+    [name, email, profile_image, password]
+  );
+  res.status(201).json({"message" : "userCreated"});
 })
 
 const server = http.createServer(app);
