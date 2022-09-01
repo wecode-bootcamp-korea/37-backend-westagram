4444146d0b38e46dddba05446265f56c8f39359b
whrod <whrodtube@gmail.com>
2022-09-01 16:48:18 +0900

    [조경찬-teamB] Assignment 2: Express API - 유저 회원가입 하기

--- teamB/kyoungchanCho/app.js
+++ teamB/kyoungchanCho/app.js
@@ -36,6 +36,22 @@ app.get("/ping", (req, res) => {
     res.json({ message : "pong"})
 })
 
+app.post("/users", async (req, res, next) => {
+    const { name, email, profile_image, password } = req.body
+
+    await database.query(
+        `INSERT INTO users(
+            name,
+            email,
+            profile_image,
+            password
+        ) VALUES (?, ?, ?, ?);
+        `,
+        [ name, email, profile_image, password ]
+    );
+    res.status(201).json({ message : "userCreated" })
+})
+
 const start = async () => {
     server.listen(PORT, () => console.log(`server is listening on ${PORT}`))
 }
