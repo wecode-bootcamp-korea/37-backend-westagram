686ffbc8cc999da9cc40433bf659c27d00785714
es <mascoeskim@gmail.com>
2022-09-02 07:15:47 +0900

    과제2-prettier적용

--- teamB/eungSooKim/app.js
+++ teamB/eungSooKim/app.js
@@ -1,57 +1,59 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const http = require("http");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const morgan = require("morgan");
-
-const { DataSource } = require('typeorm');
+const { DataSource } = require("typeorm");
 
 const database = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
-})
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
 
-database.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    });
-    
-
+database
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny'));
-const server = http.createServer(app)
+app.use(morgan("tiny"));
+
+const server = http.createServer(app);
 const PORT = process.env.PORT;
 
-
 app.get("/ping", (req, res) => {
-    res.json({ message : "pong"});
-})
+  res.json({ message: "pong" });
+});
 
 app.post("/users", async (req, res, next) => {
-    const { name, email, password } = req.body
-   
-    await database.query(
-        `INSERT INTO users(
+  const { name, email, password } = req.body;
+
+  await database.query(
+    `INSERT INTO users(
             name,
             email,
             password
         ) VALUES (?, ?, ?);
         `,
-        [ name, email, password ]
-    ); 
-     res.status(201).json({ message : "userCreated" });
-    })
+    [name, email, password]
+  );
+  res.status(201).json({ message: "userCreated" });
+});
 
 const start = async () => {
-    server.listen(PORT, () => console.log(`server is listening on ${PORT}`))
-}
+  server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+};
 
-start()
+start();
