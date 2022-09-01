1f25ad9514ad2de003528c7d41d0603b47f7aab0
최진우 <choiramsey8@gmail.com>
2022-09-01 17:25:26 +0900

    assignment1_1

--- teamA/jinwoo/app.js
+++ teamA/jinwoo/app.js
@@ -1,14 +1,14 @@
+require("dotenv").config()
+
 const http = require("http");
+
 const express = require("express");
 const cors = require("cors");
+const app = express();
 const morgan = require("morgan");
-
-const dotenv = require("dotenv");
-dotenv.config()
-
 const { DataSource } = require('typeorm')
 
-const myDataSource = new DataSource({
+const mysqlDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
@@ -17,17 +17,19 @@ const myDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE
 })
 
-myDataSource.initialize()
+mysqlDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!")
   })
-
-app = express();
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err)
+  })
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+// health check
 app.get("/ping", (req, res) => {
   res.json({ message : "pong" });
 })
