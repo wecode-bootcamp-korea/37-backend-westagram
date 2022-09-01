87503a59a019aab9d72de40e796e2ee3a9138217
dyjeong576 <dyjeong576@gmail.com>
2022-09-01 10:57:51 +0900

    첫 번째 수정 완료!

--- teamC/JeongDayoung/app.js
+++ teamC/JeongDayoung/app.js
@@ -1,16 +1,31 @@
-// require(“dotenv”).config();
+dotenv.config();
+// Build-in package
 const http = require("http");
+
+// 3rd-party package
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { DataSource } = require("typeorm");
 
 const app = express();
-
 app.use(express.json());
 app.use(morgan('dev'));
 
-dotenv.config();
+const myDataSource = new DataSource({
+  type : process.env.TYPEORM_CONNECTION,
+  host : process.env.TYPEORM_HOST,
+  port : process.env.TYPEORM_PORT,
+  username : process.env.TYPEORM_USERNAME,
+  password : process.env.TYPEORM_PASSWORD,
+  database : process.env.TYPEORM_DATABASE
+}) 
+
+myDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  });
 
 app.get('/ping', cors(), function(req, res, next){
   res.json({message : 'pong'})
