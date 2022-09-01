168ebb52d054b7efc43751b346cfa19a7b8fee47
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-09-02 01:07:29 +0900

    CRUD 구현

--- teamB/junhyungcho/app.js
+++ teamB/junhyungcho/app.js
@@ -75,6 +75,7 @@ app.post('/posts', async (req, res, next) => {
     res.status(201).json({ message : "postingCreated" });
 });
 
+// Get all posts
 app.get('/posts', async (req, res) => {
     await database.query(
         `SELECT
@@ -85,13 +86,59 @@ app.get('/posts', async (req, res) => {
         FROM posts p
         `,(err, rows) => {
                 res.status(200).json(rows);
-        });
+    });
+})
+
+//Get all posts along with users
+app.get('/users', async (req, res) => {
+    await database.query(
+        `SELECT
+            posts.id, 
+            posts.title, 
+            posts.description, 
+            posts.cover_image, 
+            users.first_name, 
+            users.last_name, 
+            users.age 
+        FROM likes l 
+        INNER JOIN users ON l.users_id = users.id 
+        INNER JOIN posts ON l.posts_id = posts.id
+        `, (err, rows) => {
+            res.status(200).json(rows);
+    });
+});
+
+//Update a single user by its primary key
+app.patch('/posts', async(req, res) => {
+    const { title, description, coverImage, postId } = req.body;
+    
+    await database.query(
+        `UPDATE posts
+        SET
+            title = ?,
+            description = ?,
+            cover_image = ?
+        WHERE id = ?
+        `,
+        [ title, description, coverImage, postId ]
+    );
+        res.status(201).json({ message : "successfully updated" });
+});
+
+//Delete a post
+app.delete('/posts/:postId', async(req, res) => {
+    const { postId } = req.params;
+
+    await database.query(
+        `DELETE FROM posts
+        WHERE posts.id = ${postId}`
+    );
+        res.status(204).json({ message : "successfully deleted" });
 })
 
 // start server
 const start = async () => {
     try {
-        // app.listen(PORT, () => console.log(`===This is app===\r\n\n${app}\r\n\n===This is express===\r\n\n${express}\r\n`));
         server.listen(PORT, () => console.log(`====Server is listening on ${PORT}====`));
     } catch (err) {
         throw err; //상위 컨텍스트로 에러 전파
--- teamB/junhyungcho/db/migrations/20220901082858_create_likes_table.sql
+++ teamB/junhyungcho/db/migrations/20220901082858_create_likes_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE likes (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     users_id INT NOT NULL,
     posts_id INT NOT NULL,
-    CONSTRAINT post_id_fkey FOREIGN KEY (posts_id) REFERENCES posts(id),
-    CONSTRAINT user_id_fkey FOREIGN KEY (users_id) REFERENCES users(id)
+    CONSTRAINT user_id_fkey FOREIGN KEY (users_id) REFERENCES users.id,
+    CONSTRAINT post_id_fkey FOREIGN KEY (posts_id) REFERENCES posts.id
 );
 
 -- migrate:down
