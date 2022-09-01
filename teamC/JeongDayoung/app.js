a79e9d5ca83fdc020021c64a74bb594baa5e44f4
dyjeong576 <dyjeong576@gmail.com>
2022-09-01 13:13:40 +0900

    첫 번째 수정 완료!

--- teamC/JeongDayoung/app.js
+++ teamC/JeongDayoung/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();// env를 불러오는 함수임으로 port 선언 전에 들어와야 한다!
+
 const http = require("http");
 const express = require("express");
 const morgan = require("morgan");
@@ -6,13 +8,13 @@ const dotenv = require("dotenv");
 const { DataSource } = require("typeorm");
 const app = express();
 const server = http.createServer(app);
-dotenv.config(); // env를 불러오는 함수임으로 port 선언 전에 들어와야 한다!
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
   type : process.env.TYPEORM_CONNECTION,
   host : process.env.TYPEORM_HOST,
   port : process.env.TYPEORM_PORT,
@@ -21,7 +23,7 @@ const myDataSource = new DataSource({
   database : process.env.TYPEORM_DATABASE
 }) 
 
-myDataSource.initialize()
+appDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
   })
@@ -29,21 +31,24 @@ myDataSource.initialize()
     console.error("Error during Data Source initialization", err)
   });
 
-app.get('/ping', cors(), function(req, res, next){
+app.get('/ping', function(req, res, next){
   res.json({message : 'pong'})
 })
 
 app.post("/users", async(req, res, next) => {
   const {name, email, profile_image, password} = req.body; 
-  await myDataSource.query(
-    `INSERT INTO users(name, email, profile_image, password)
-    values(?, ?, ?, ?);`,
+  
+  await appDataSource.query(
+    `INSERT INTO users(
+      name, 
+      email, 
+      profile_image, 
+      password
+      )values(?, ?, ?, ?);`,
     [name, email, profile_image, password]
   );
   res.status(201).json({"message" : "userCreated"});
 })
-
-
 
 const start = async () => {
   server.listen(PORT, () => console.log(`erviser is listening on  ${PORT}`))
