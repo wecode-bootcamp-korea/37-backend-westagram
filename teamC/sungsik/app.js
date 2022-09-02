7554939fea2854bf177a8d931025256a183287f7
walter811 <kimsungsik@kimsungsik.local>
2022-09-02 13:37:47 +0900

    [ADD] Express 초기 환경세팅

---
+++ teamC/sungsik/app.js
@@ -0,0 +1,45 @@
+// dotenv
+require("dotenv").config()
+
+// Built-in package
+const http = require("http");
+
+// 3rd-party package
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const morgan = require("morgan");
+
+const { DataSource } = require("typeorm")
+
+const database = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+})
+
+database.initialize()
+
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+app.get('/ping', (req, res) => {
+    res.status(200).json({ message:'pong' })
+})
+
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
+const start = async() => {
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`))
+}
+
+start();
---
+++ teamC/sungsik/db/migrations/20220902043124_users.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+create table users (
+    id int not null auto_increment,
+    name varchar(50) not null,
+    email varchar(200) not null,
+    profile_image varchar(1000) null,
+    password varchar(200) not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp null on update current_timestamp,
+    primary key (id)
+);
+
+-- migrate:down
+drop table users;
