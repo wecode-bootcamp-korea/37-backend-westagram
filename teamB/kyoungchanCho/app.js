4ee33607dfcd882c0059fd54227ea1d0c8b97190
whrod <whrodtube@gmail.com>
2022-09-03 22:55:28 +0900

    [조경찬-teamB] Assignment 3: Express API - CRUD 과제 3-8 기능 구현 likes 테이블 user_id, post_id CONSTRAINT UNIQUE 추가 오타 수정

--- teamB/kyoungchanCho/app.js
+++ teamB/kyoungchanCho/app.js
@@ -12,7 +12,7 @@ const { ppid } = require('process');
 
 const database = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,s
+    host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
     username: process.env.TYPEORM_USERNAME,
     password: process.env.TYPEORM_PASSWORD,
