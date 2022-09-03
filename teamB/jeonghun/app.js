f8ffa998e10606ee3f50d487a96760e4f301e6d2
ninetysixHoon <789ee45@gmail.com>
2022-09-03 21:19:12 +0900

    2번 백업용

--- teamB/jeonghun/app.js
+++ teamB/jeonghun/app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 
 const {DataSource} = require('typeorm');
 
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
@@ -15,7 +15,7 @@ const myDataSource = new DataSource({
     database: process.env.TYPEORM_DATABASE
 });
 
-myDataSource.initialize()
+appDataSource.initialize()
  .then(() => {
   console.log('Data Source has been initialized!')  
  })
@@ -25,26 +25,99 @@ myDataSource.initialize()
 
 const app = express()
 
+const server = http.createServer(app)
+const PORT = process.env.PORT;
+
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-app.get('/ping', cors(), function (req, res, next) {
-    res.json({message: 'pong'});
+app.get('/ping', function (req, res, next) {
+    res.status(201).json({message: 'pong'});
 });
 
-const server = http.createServer(app)
-const PORT = process.env.PORT;
+app.post('/user/sign', async (req, res, next) => {
+  const { name, email, profileImage, password } = req.body
+
+  // console.log(req)
+
+  await appDataSource.query(
+    `INSERT INTO users(
+      name,
+      email,
+      profile_image,
+      password
+      ) VALUES (?, ?, ?, ?);
+      `,
+      [ name, email, profileImage, password ]
+    );
+     res.status(201).json({ message : 'userCreated'});
+    })
+
+    app.post('/post', async (req, res, next) => {
+      const { title, content } = req.body
+    
+      // console.log(req)
+    
+      await appDataSource.query(
+        `INSERT INTO posts(
+          title,
+          content
+          ) VALUES (?, ?);
+          `,
+          [ title, content ]
+        );
+         res.status(201).json({ message : 'successfully created'});
+        })    
+
+        app.post('/like', async (req, res, next) => {
+          const { userid, postid } = req.body
+        
+          await appDataSource.query(
+            `INSERT INTO likes(
+              user_id,
+              post_id
+              ) VALUES (?, ?);
+              `,
+              [ userid, postid ]
+            );
+             res.status(201).json({ message : 'successfully created'});
+            })   
+
+app.get('/user', async (req, res) => {
+  await appDataSource.manager.query(
+    `SELECT
+         b.id,
+         b.title,
+         b.description,
+         b.cover_image
+        FROM users b`
+    ,(err, rows) => {
+         res.status(200).json(rows);
+    })
+});
+
+app.get('/users-with-posts', async (req, res) => {
+  await appDataSource.manager.query(
+    `SELECT
+         users.id,
+         users.title,
+         users.description,
+         users.cover_image,
+         posts.first_name,
+         posts.last_name,
+         posts.age
+        FROM users_posts ba
+        INNER JOIN posts ON ba.post_id = posts.id
+        INNER JOIN users ON ba.user_id = users.id`
+    ,(err, rows) => {
+         res.status(200).json(rows);
+    })
+});
+
 
 const start = async () => {
   server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
 }
 
 start()
-
-
-
-
-
-
- 
