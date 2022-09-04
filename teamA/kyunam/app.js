13263d8767229717a49fbac248332026a0ed1399
k <k@K.local>
2022-09-05 08:35:04 +0900

    [배규남-teamA] Assignment 3: Express API - CRUD

--- teamA/kyunam/app.js
+++ teamA/kyunam/app.js
@@ -32,9 +32,38 @@ app.get('/ping', (req, res, next) => {
   res.status(200).json({ message: 'pong' });
 });
 
+app.get('/posts', async (req, res, next) => {
+  const posts = await appDataSource.query(
+    `SELECT * 
+     FROM posts
+     `
+  );
+  res.status(200).json({ data: posts });
+});
+
+app.get('/users/:userId', async (req, res, next) => {
+  const userId = req.params.userId;
+  const user = await appDataSource.query(
+    `SELECT u.id, u.profile_image 
+       FROM users u
+       WHERE u.id = ?
+      `,
+    [userId]
+  );
+  const post = await appDataSource.query(
+    `SELECT p.id, p.title, p.content 
+       FROM posts p join users u
+       ON p.user_id = u.id and u.id= ?
+      `,
+    [userId]
+  );
+  user[0].postings = post;
+  res.status(200).json({ data: user[0] });
+});
+
 app.post('/users', async (req, res, next) => {
   const { name, email, profile_image, password } = req.body;
-  appDataSource.manager.query(
+  await appDataSource.manager.query(
     `INSERT INTO users(
         name,
         email,
@@ -45,6 +74,62 @@ app.post('/users', async (req, res, next) => {
     [name, email, profile_image, password]
   );
   res.status(201).json({ message: 'userCreated' });
+});
+
+app.post('/posts', async (req, res, next) => {
+  const { title, content, user_id } = req.body;
+  await appDataSource.query(
+    `INSERT INTO posts(
+        title,
+        content,
+        user_id
+      ) VALUES (?, ?, ?)
+      `,
+    [title, content, user_id]
+  );
+  res.status(201).json({ message: 'postCreated' });
+});
+
+app.post('/likes', async (req, res, next) => {
+  const { userId, postId } = req.body;
+  await appDataSource.query(
+    `INSERT INTO likes(
+        user_id,
+        post_id
+        )VALUES(?, ?)
+      `,
+    [userId, postId]
+  );
+  res.status(201).json({ message: 'likeCreated' });
+});
+
+app.patch('/posts', async (req, res, next) => {
+  const { content } = req.body;
+  await appDataSource.query(
+    `UPDATE posts
+       SET content= ?
+       WHERE user_id=1 and id=1
+      `,
+    [content]
+  );
+  const post = await appDataSource.query(
+    `SELECT u.id as userId, u.name, p.id, p.title, p.content
+       FROM users u join posts p 
+       ON u.id = p.user_id and p.id=1
+      `
+  );
+  res.status(200).json({ data: post[0] });
+});
+
+app.delete('/posts/:postId', async (req, res, next) => {
+  const postId = req.params.postId;
+  await appDataSource.query(
+    `DELETE FROM posts
+      WHERE id = ?
+      `,
+    [postId]
+  );
+  res.status(200).json({ message: 'postingDeleted' });
 });
 
 app.listen(process.env.PORT, () => {
