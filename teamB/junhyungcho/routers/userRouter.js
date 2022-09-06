69943afcd055ac65bce1d5d010d64e3e49280150
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-09-07 03:52:48 +0900

    레이아웃구현

--- teamB/junhyungcho/routers/userRouter.js
+++ teamB/junhyungcho/routers/userRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { userController } = require('../controllers');
 
-const router = express.Router(); //app, 즉 express()의 router 기능만 구현하는 코드
+const router = express.Router(); 
 
-router.post('/signup', userController.signUp); // 발행된 토큰을 확인하는 곳
+router.post('/signup', userController.signUp);
 router.get('/post/:userId', userController.postList);
 
 module.exports = router;
