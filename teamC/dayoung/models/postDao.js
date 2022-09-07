1486e38c8bec1ddd0ed69a2354addf1e6d473e89
dyjeong576 <dyjeong576@gmail.com>
2022-09-07 20:39:20 +0900

    Datasource.js 구현 및 console 제거

--- teamC/dayoung/models/postDao.js
+++ teamC/dayoung/models/postDao.js
@@ -1,24 +1,4 @@
-const { DataSource } = require('typeorm');
-
-const myDataSource = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
-})
-
-myDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error occurred during Data Source initialization", err);
-	  myDataSource.destroy();
-  });
-
-  /*********************************************************** */
+const myDataSource = require("./DataSource");
 
 const createPost = async ( title, content, userId ) => {
 	try {
