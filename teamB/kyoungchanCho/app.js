bcc6a7972e9c9d4a3849fa70fbf620de4ea83555
whrod <whrodtube@gmail.com>
2022-09-08 14:34:10 +0900

    레이어드 패턴 & 인증,인가 에러 핸들링 추가 예정 주석 삭제

--- teamB/kyoungchanCho/app.js
+++ teamB/kyoungchanCho/app.js
@@ -8,28 +8,10 @@ const morgan = require("morgan");
 const route = require("./routes");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
-// const { DataSource } =require('typeorm');
-
-// const database = new DataSource({
-//     type: process.env.TYPEORM_CONNECTION,
-//     host: process.env.TYPEORM_HOST,
-//     port: process.env.TYPEORM_PORT,
-//     username: process.env.TYPEORM_USERNAME,
-//     password: process.env.TYPEORM_PASSWORD,
-//     database: process.env.TYPEORM_DATABASE
-// })
 
 app = express()
 const server = http.createServer(app);
 const PORT = process.env.PORT;
-
-// database.initialize()
-//     .then(() => {
-//         console.log("Data Source has been initialized!")
-//     })
-//     .catch((err) => {
-//         console.error("Error during Data Source initialization", err)
-//     })
 
 app.use(express.json());
 app.use(cors());
