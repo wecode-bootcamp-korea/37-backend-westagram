3d4dbb02138287654c3fc2185349ddf9a723798a
es <mascoeskim@gmail.com>
2022-09-08 12:57:51 +0900

    utils

--- teamB/eungSooKim/models/userDao.js
+++ teamB/eungSooKim/models/userDao.js
@@ -1,22 +1,4 @@
-const { DataSource } = require("typeorm");
-
-const database = new DataSource({
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-});
-
-database
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
+const { database } = require("./data-source");
 
 const signUp = async (name, email, password, profileImage) => {
   try {
@@ -31,7 +13,7 @@ const signUp = async (name, email, password, profileImage) => {
       [name, email, password, profileImage]
     );
   } catch (err) {
-    const error = new Error("INVALID_DATA_INPUT");
+    const error = new Error("duplicated email");
     error.statusCode = 500;
     throw error;
   }
--- teamB/eungSooKim/services/userService.js
+++ teamB/eungSooKim/services/userService.js
@@ -1,14 +1,9 @@
 const userDao = require("../models/userDao");
+const { validatePassword, validateEmail } = require("../utils/validators");
 
 const signUp = async (name, email, password, profileImage) => {
-  const pwValidation = new RegExp(
-    "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})"
-  );
-  if (!pwValidation.test(password)) {
-    const err = new Error("INVALID PASSWORD");
-    err.statusCode = 400;
-    throw err;
-  }
+  validatePassword(password);
+  validateEmail(email);
   const signUp = await userDao.signUp(name, email, password, profileImage);
   return signUp;
 };
